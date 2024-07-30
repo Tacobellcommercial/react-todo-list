04f251108cb20142fc0f239d2bad47ddc857bb3c
Loser <tacobellcommercial@gmail.com>
2024-07-30 08:50:21 -0500

    Dependency change

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -25,7 +25,7 @@ function Register(){
     return(
         <form className="register" onSubmit={async (e)=>{
             e.preventDefault();
-            const response = await fetch("http://localhost:3001/create-new-user", {
+            const response = await fetch("https://todo-list-api-ey6p.onrender.com/add-todo-item", {
                 method: "POST",
                 headers:{
                     "Content-Type": "application/json"
